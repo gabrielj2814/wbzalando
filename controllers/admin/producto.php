d751d41bc65f816fd1603262a405304151717938
gabriel valera <hashiramakage@gmail.com>
2022-01-17 11:10:21 -0400

    removiendo comentarios en la función ajaxProcessGetConsultarEsquemaProducto

--- controllers/admin/producto.php
+++ controllers/admin/producto.php
@@ -267,38 +267,9 @@ class ProductoController extends ModuleAdminController{
     }
 
     public function ajaxProcessGetConsultarEsquemaProducto(){
-        // $respuesta_servidor=["respuestaServidor" => [], "codigo_respuesta" => 0];
-        // $idComerciante=Configuration::get("WB_ZALANDO_ID_COMERCIANTE");
-        // $endPoint=Configuration::get("WB_ZALANDO_END_POINT");
-        // $token=Configuration::get("WB_ZALANDO_TOKEN_ACCESO");
-        // $url=$endPoint."/merchants/".$idComerciante."/outlines/".$_POST["esquema"];
-        // $curlController=new CurlController($url);
-        // $header = array(
-        //     'Authorization: '.'Bearer '. $token
-        // );
-        // $curlController->setdatosCabezera($header);
-        // $respuesta=$curlController->ejecutarPeticion("get",false);
-        // $respuesta_servidor["respuestaServidor"]=[
-        //     "model"=> [
-        //         "mandatory_types" =>$respuesta["response"]->tiers->model->mandatory_types,
-        //         "optional_types" =>$respuesta["response"]->tiers->model->optional_types
-        //     ],
-        //     "config"=> [
-        //         "mandatory_types" =>$respuesta["response"]->tiers->config->mandatory_types,
-        //         "optional_types" =>$respuesta["response"]->tiers->config->optional_types
-        //     ],
-        //     "simple"=> [
-        //         "mandatory_types" =>$respuesta["response"]->tiers->simple->mandatory_types,
-        //         "optional_types" =>$respuesta["response"]->tiers->simple->optional_types
-        //     ]
-        // ];
-        // error_log("respuesta al consultar un esquema de producto zalando =>>>>  " . var_export($respuesta["response"], true));
-        // $respuesta_servidor["codigo_respuesta"]=$respuesta["estado"];
-        // print(json_encode($respuesta_servidor));
         $respuesta_servidor=["respuestaServidor" => []];
         $resultEsquemas=$this->chequearEsquemasDeHoyDB();
         $resultEsquemas[0]["esquemas_full"]=json_decode($resultEsquemas[0]["esquemas_full"]);
-        // $_POST["esquema"]
         $datosEsquemaProducto=[];
         foreach($resultEsquemas[0]["esquemas_full"] as $esquema){
             $esquema=(object)$esquema;
