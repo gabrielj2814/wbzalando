2d5f8cb566fc2f8a11895868e51ca5ae6aa224c0
gabriel valera <hashiramakage@gmail.com>
2022-02-16 13:53:47 -0400

    agregando listar, eliminar y al eliminar o agregar actualiza los atributos guardados

--- views/js/atributotalla.js
+++ views/js/atributotalla.js
@@ -8,6 +8,7 @@ let botonConsultarTallas=document.getElementById("botonConsultarTallas");
 
 function registrar(){
     const linkControlador=document.getElementById("linkControlador").value;
+    let atributo=document.getElementById("selectAtributoPrestashop")
     $.ajax({
         type: 'POST',
         cache: false,
@@ -16,10 +17,11 @@ function registrar(){
         data: {
             ajax: true,
             action: 'postguardaratributoTalla',
-            id_attribute:"1"
+            id_attribute:atributo.value
         },
         success: (respuesta) => {
             console.log(respuesta);
+            consultarTodos()
             // let datos=JSON.parse(JSON.stringify(respuesta.datos))
             // console.log("productos filtrados =>>> ",datos)
         },
@@ -41,12 +43,25 @@ function consultarAtributosPrestashop(){
         },
         success: (respuesta) => {
             console.log(respuesta);
-            // let datos=JSON.parse(JSON.stringify(respuesta.datos))
-            // console.log("productos filtrados =>>> ",datos)
+            let datos=JSON.parse(JSON.stringify(respuesta.respuestaServidor))
+            insertarAtributoSelect(datos)
+            consultarTodos()
         },
         error: () => {
         }
     });
+}
+
+function insertarAtributoSelect(atributos){
+    let selectAtributoPrestashop=document.getElementById("selectAtributoPrestashop")
+    selectAtributoPrestashop.innerHTML="";
+    let html="";
+    for(let atributo of atributos){
+        let option="<option value='"+atributo.id_attribute_group+"'>"+atributo.name+"</option>"
+        html+=option
+    }
+    selectAtributoPrestashop.innerHTML=html;
+
 }
 
 function consultarTodos(){
@@ -62,12 +77,32 @@ function consultarTodos(){
         },
         success: (respuesta) => {
             console.log(respuesta);
-            // let datos=JSON.parse(JSON.stringify(respuesta.datos))
+            let datos=JSON.parse(JSON.stringify(respuesta.respuestaServidor.datos))
+            insertarAtributosGuardados(datos);
             // console.log("productos filtrados =>>> ",datos)
         },
         error: () => {
         }
     });
+}
+
+function insertarAtributosGuardados(atributos){
+    let listaDeAtributosGuardados=document.getElementById("listaDeAtributosGuardados")
+    listaDeAtributosGuardados.innerHTML="";
+    let html="";
+    for(let atributo of atributos){
+        let option="\
+        <div class='col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xs-12 modal-header p-20 alignitem-tb'>\
+            <div class='col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3 col-xs-3 txt-title'>\
+                <label>"+atributo.name+"</label>\
+            </div>\
+            <div class='col-3 col-sm-2 col-md-2 col-lg-2 col-xl-3 col-xs-2 text-left'>\
+                <button style='border: unset;' id='"+atributo.id_atributo_talla+"' onClick='eliminar(this)'><svg width='24' height='24' fill='currentColor' class='bi bi-trash3' viewBox='0 0 16 16'><path d='M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z'/></svg></button>\
+            </div>\
+        </div>"
+        html+=option
+    }
+    listaDeAtributosGuardados.innerHTML=html;
 }
 
 function consultar(){
@@ -116,7 +151,7 @@ function actualizar(){
     });
 }
 
-function eliminar(){
+function eliminar(a){
     const linkControlador=document.getElementById("linkControlador").value;
     $.ajax({
         type: 'POST',
@@ -126,34 +161,12 @@ function eliminar(){
         data: {
             ajax: true,
             action: 'geteliminar',
-            id_atributo_talla:2
+            id_atributo_talla:a.id
 
         },
         success: (respuesta) => {
             console.log(respuesta);
-            // let datos=JSON.parse(JSON.stringify(respuesta.datos))
-            // console.log("productos filtrados =>>> ",datos)
-        },
-        error: () => {
-        }
-    });
-}
-
-function actualizar(){
-    const linkControlador=document.getElementById("linkControlador").value;
-    $.ajax({
-        type: 'POST',
-        cache: false,
-        dataType: 'json',
-        url: linkControlador, 
-        data: {
-            ajax: true,
-            action: 'postactualizar',
-            asociacion: datosTest
-
-        },
-        success: (respuesta) => {
-            console.log(respuesta);
+            consultarTodos()
             // let datos=JSON.parse(JSON.stringify(respuesta.datos))
             // console.log("productos filtrados =>>> ",datos)
         },
@@ -166,8 +179,8 @@ consultarAtributosPrestashop()
 
 
 botonRegistrar.addEventListener("click",registrar)
-botonConsultarTodos.addEventListener("click",consultarTodos)
-botonConsultar.addEventListener("click",consultar)
-botonActualizar.addEventListener("click",actualizar)
-botonEliminar.addEventListener("click",eliminar)
-botonConsultarTallas.addEventListener("click",traerTallas)
+// botonConsultarTodos.addEventListener("click",consultarTodos)
+// botonConsultar.addEventListener("click",consultar)
+// botonActualizar.addEventListener("click",actualizar)
+// botonEliminar.addEventListener("click",eliminar)
+// botonConsultarTallas.addEventListener("click",traerTallas)
