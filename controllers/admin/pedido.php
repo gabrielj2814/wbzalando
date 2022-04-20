b5e97384ec18af87fb214b7c3d8e8bf8f6884f31
gabriel valera <hashiramakage@gmail.com>
2022-04-20 15:46:33 -0400

    poniendo en marcha la vista pedido

--- controllers/admin/pedido.php
+++ controllers/admin/pedido.php
@@ -29,5 +29,22 @@ class PedidoController extends ModuleAdminController{
         $this->setTemplate('/pedido/vista.tpl');
     }
 
+    public function ajaxProcessGetConsultarPedidosZalando(){
+        $respuesta_servidor=["respuestaServidor" => [],"estatuRespuestaApi" => 0];
+        $idComerciante=Configuration::get("WB_ZALANDO_ID_COMERCIANTE");
+        $endPoint=Configuration::get("WB_ZALANDO_END_POINT");
+        $token=Configuration::get("WB_ZALANDO_TOKEN_ACCESO");
+        $url=$endPoint."/merchants/".$idComerciante."/orders";
+        $curlController=new CurlController($url);
+        $header = array(
+            'Authorization: '.'Bearer '. $token
+        );
+        $curlController->setdatosCabezera($header);
+        $respuesta=$curlController->ejecutarPeticion("get",false);
+        $respuesta_servidor["respuestaServidor"]=$respuesta["response"];
+        $respuesta_servidor["estatuRespuestaApi"]=$respuesta["estado"];
+        print(json_encode($respuesta));
+    }
+
 
 }
--- views/js/pedido.js
+++ views/js/pedido.js
@@ -1,1 +1,25 @@
 console.log("print('hola en mundo en python pero es realmente en javascript XD')")
+
+
+function consultarPedidosZalando(){
+    const linkControlador=document.getElementById("linkControlador").value;
+    $.ajax({
+        type: 'GET',
+        cache: false,
+        dataType: 'json',
+        url: linkControlador, 
+        data: {
+            ajax: true,
+            action: 'getconsultarpedidoszalando'
+        },
+        success: (respuesta) => {
+            console.log(respuesta);
+            // let datos=JSON.parse(JSON.stringify(respuesta.datos))
+            // console.log("productos filtrados =>>> ",datos)
+        },
+        error: () => {
+        }
+    });
+}
+
+consultarPedidosZalando()
